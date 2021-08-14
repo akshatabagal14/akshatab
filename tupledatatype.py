f92fbcf950bdf13e36e6242e24d5afffc77762de
akshatabagal14 <88831354+akshatabagal14@users.noreply.github.com>
2021-08-14 13:55:41 +0530

    Add files via upload

---
+++ tupledatatype.py
@@ -0,0 +1,15 @@
+#write a program to manuplate tuple data
+y=[1,2,'python',4,5,'list',7,'datatype',9,10]
+print("Tuple y:",y)
+
+#extract index number 2 to 5
+print("list y: 2 to 5", y[1:6]) 
+
+#print list elements reverse
+print("list y in reverse:",y[::-1])
+
+#use index and count function
+#index function
+print("index of python",y.index('python'))
+#count function
+print("index of list",y.index('list'))
