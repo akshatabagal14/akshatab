e16e09e85c9bec6c192c78e82b0c2bb490564f07
akshatabagal14 <88831354+akshatabagal14@users.noreply.github.com>
2021-08-28 20:07:10 +0530

    Add files via upload

---
+++ employeeid_card.py
@@ -0,0 +1,21 @@
+#Write a program to generate employee ID-Card(image not required,id card show on console) 
+print("--------------------***COMPANY-IDCARD***---------------------")
+print("-------------------------------------------------------------")
+print("Employee Name: Deshmukh Ovi Viraj")
+print("GEnder: Female")
+print("Date Of Birth: 22/01/2002")
+print("Blood group: B+")
+print("Phone No: 1234567890")
+print("Address: damaninagar,solapur")
+print("Email: ovideshmukh2002@gmail.com")
+print("--------------------------------------------------------------")
+print("--------------------------------------------------------------")
+
+
+
+
+
+
+
+
+
